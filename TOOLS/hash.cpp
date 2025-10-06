e8dc625b2e07fae69f2ccd9d07b7cbca4d022788
Martin Gäckler <git@gaeckler.at>
2025-10-06 11:02:12 +0200

    option F introduced

--- TOOLS/hash.cpp
+++ TOOLS/hash.cpp
@@ -3,10 +3,10 @@
 		Module:			hash.cpp
 		Description:	calculate the hash of a file
 		Author:			Martin Gäckler
-		Address:		Hopfengasse 15, A-4020 Linz
+		Address:		Hofmannsthalweg 14, A-4030 Linz
 		Web:			https://www.gaeckler.at/
 
-		Copyright:		(c) 1988-2021 Martin Gäckler
+		Copyright:		(c) 1988-2025 Martin Gäckler
 
 		This program is free software: you can redistribute it and/or modify  
 		it under the terms of the GNU General Public License as published by  
@@ -15,7 +15,7 @@
 		You should have received a copy of the GNU General Public License 
 		along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-		THIS SOFTWARE IS PROVIDED BY Martin Gäckler, Germany, Munich ``AS IS''
+		THIS SOFTWARE IS PROVIDED BY Martin Gäckler, Linz, Austria ``AS IS''
 		AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
 		TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
 		PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR
@@ -28,7 +28,6 @@
 		OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 		SUCH DAMAGE.
 */
-
 
 // --------------------------------------------------------------------- //
 // ----- switches ------------------------------------------------------ //
@@ -72,6 +71,7 @@ static const int UPDATE			= 0x010;
 static const int SILENT			= 0x020;
 static const int HASH_FILE		= 0x040;
 static const int EXECUTABLES	= 0x080;
+static const int FORCE_UPDATE	= 0x100;
 
 static const uint32 magic = ('h'<<24) | ('a'<<16) | ('s'<<8) | 'h';
 static const uint16 version = 2;
@@ -86,6 +86,9 @@ static const uint16 version = 2;
 
 struct Digests
 {
+	uint64				fileSize;
+	uint64				modifyUTC;
+
 	MD5Hash::Digest		md5Digest;
 	SHA224Hash::Digest	sha224Digest;
 	SHA256Hash::Digest	sha256Digest;
@@ -94,11 +97,13 @@ struct Digests
 
 	bool operator == ( const Digests &oper )
 	{
-		return md5Digest == oper.md5Digest
-		&& sha224Digest == oper.sha224Digest
-		&& sha256Digest == oper.sha256Digest
-		&& sha384Digest == oper.sha384Digest
-		&& sha512Digest == oper.sha512Digest;
+		return fileSize == oper.fileSize		&&
+			modifyUTC == oper.modifyUTC			&&
+			md5Digest == oper.md5Digest			&&
+			sha224Digest == oper.sha224Digest	&&
+			sha256Digest == oper.sha256Digest	&&
+			sha384Digest == oper.sha384Digest	&&
+			sha512Digest == oper.sha512Digest;
 	}
 	bool operator != ( const Digests &oper )
 	{
@@ -114,6 +119,8 @@ struct Digests
 	}
 };
 
+typedef TreeMap<F_STRING, Digests>	AllDigestdMap;
+
 // --------------------------------------------------------------------- //
 // ----- class definitions --------------------------------------------- //
 // --------------------------------------------------------------------- //
@@ -128,7 +135,8 @@ struct Digests
 
 static gak::CommandLine::Options options[] =
 {
-	{ 'U', "update",		0, 1, UPDATE },
+	{ 'U', "update",		0, 1, UPDATE,		"update the hash file if hash and time has changed" },
+	{ 'F', "force",			0, 1, FORCE_UPDATE,	"allow update the hash file entry if hash has changed, only" },
 	{ 'S', "silent",		0, 1, SILENT },
 	{ 'E', "excecutables",	0, 1, EXECUTABLES },
 	{ 'H', "hashFile",		0, 1, HASH_FILE|gak::CommandLine::needArg, "hash file" },
@@ -148,13 +156,17 @@ static gak::CommandLine::Options options[] =
 // --------------------------------------------------------------------- //
 
 static void hash( 
-	const STRING &fileName, bool silent, bool update, TreeMap<F_STRING, Digests> &allDigests 
+	const STRING &fileName, bool silent, bool update, bool forceUpdate, AllDigestdMap &allDigests 
 )
 {
 	ArrayOfData	buffer;
 	readFromFile( &buffer, fileName );
+	DirectoryEntry	dirEntry( fileName );
 
 	Digests	digests;
+
+	digests.fileSize = dirEntry.fileSize;
+	digests.modifyUTC = dirEntry.modifiedDate.getUtcUnixSeconds();
 
 	{
 		MD5Hash	md5;
@@ -206,10 +218,11 @@ static void hash(
 	{
 		if( allDigests[fileName] != digests )
 		{
-			std::cerr << "File has changed hash " << fileName << std::endl;
-			if( update )
+			std::cerr << "File has changed hash, date or time " << fileName << std::endl;
+			if( update && (allDigests[fileName].modifyUTC != digests.modifyUTC || forceUpdate) )
 			{
 				allDigests[fileName] = digests;
+				std::cerr << "\t\t... updated" << std::endl;
 			}
 		}
 	}
@@ -219,7 +232,7 @@ static void hash(
 	}
 }
 
-static void hashDirectory( const STRING &directoryName, bool silent, bool update, bool executables, TreeMap<F_STRING, Digests> &allDigests )
+static void hashDirectory( const STRING &directoryName, bool silent, bool update, bool forceUpdate, bool executables, AllDigestdMap &allDigests )
 {
 	DirectoryList	dirList;
 	F_STRING	path = directoryName;
@@ -263,12 +276,12 @@ static void hashDirectory( const STRING &directoryName, bool silent, bool update
 			|| extension == "cmd" || extension == "bat" 
 			|| extension == "ttf" || extension == "fon" )
 			{
-				hash( fileName,  silent, update, allDigests );
+				hash( fileName,  silent, update, forceUpdate, allDigests );
 			}
 		}
 		else
 		{
-			hashDirectory( fileName, silent, update, executables, allDigests );
+			hashDirectory( fileName, silent, update, forceUpdate, executables, allDigests );
 		}
 	}
 }
@@ -279,13 +292,14 @@ static int hash( const CommandLine &cmdLine )
 	const char	*arg;
 	F_STRING	fileArg;
 
-	bool		silent = cmdLine.flags & SILENT;
-	bool		update = cmdLine.flags & UPDATE;
-	bool		executables = cmdLine.flags & EXECUTABLES;
+	const bool	silent = cmdLine.flags & SILENT;
+	const bool	update = cmdLine.flags & UPDATE;
+	const bool	forceUpdate = cmdLine.flags & FORCE_UPDATE;
+	const bool	executables = cmdLine.flags & EXECUTABLES;
 
 	F_STRING	hashFile = cmdLine.flags & HASH_FILE ? cmdLine.parameter['H'][0] : NULL_STRING;
 
-	TreeMap<F_STRING, Digests>	allDigests;
+	AllDigestdMap	allDigests;
 
 	if( !hashFile.isEmpty() && isFile( hashFile ) )
 	{
@@ -298,11 +312,11 @@ static int hash( const CommandLine &cmdLine )
 
 		if( isFile( fileArg ) )
 		{
-			hash( fileArg, silent, update, allDigests );
+			hash( fileArg, silent, update, forceUpdate, allDigests );
 		}
 		else
 		{
-			hashDirectory( fileArg, silent, update, executables, allDigests );
+			hashDirectory( fileArg, silent, update, forceUpdate, executables, allDigests );
 		}
 	}
 
