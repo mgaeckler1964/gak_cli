a91c8ed7f01ac9fa028cb325e0428e97cd9ef76f
Martin Gäckler <git@gaeckler.at>
2026-02-27 10:46:54 +0100

    migrate pathenv to c++ and use registry class

--- TOOLS/pathenv.cpp
+++ TOOLS/pathenv.cpp
@@ -1,12 +1,12 @@
 /*
 		Project:		GAK_CLI
-		Module:			pathEnv.c
+		Module:			pathEnv.cpp
 		Description:	Generate environment variables for some special windows directories
 		Author:			Martin Gäckler
-		Address:		Hopfengasse 15, A-4020 Linz
+		Address:		Hofmannsthalweg 14, A-4030 Linz
 		Web:			https://www.gaeckler.at/
 
-		Copyright:		(c) 1988-2021 Martin Gäckler
+		Copyright:		(c) 1988-2026 Martin Gäckler
 
 		This program is free software: you can redistribute it and/or modify  
 		it under the terms of the GNU General Public License as published by  
@@ -15,7 +15,7 @@
 		You should have received a copy of the GNU General Public License 
 		along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-		THIS SOFTWARE IS PROVIDED BY Martin Gäckler, Germany, Munich ``AS IS''
+		THIS SOFTWARE IS PROVIDED BY Martin Gäckler, Linz, Austria ``AS IS''
 		AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
 		TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
 		PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR
@@ -29,102 +29,81 @@
 		SUCH DAMAGE.
 */
 
+#include <iostream>
+
 #include <windows.h>
 #include <gak/gaklib.h>
+#include <gak/array.h>
+#include <winlib/registry.h>
 
-static void setenv( const char *env, HKEY environmentKey )
+using namespace gak;
+using namespace winlib;
+
+static void setenv( const char *env, const Registry &environmentKey )
 {
 	const char *var	= getenv( env );
 
 	if( var && *var )
 	{
-		RegSetValueEx(
-			environmentKey,
-			env, 0, REG_EXPAND_SZ,
-			(unsigned char *)var, (DWORD)(strlen(var))
-		);
-
-		printf( "%s=%s", env, var );
+		environmentKey.setValueEx( env, rtENV, var, strlen(var)+1 );
+		std::cout << env << '=' << var << std::endl;
 	}
 }
 
-int main( void )
+int main()
 {
-	HKEY	shellFolderKey,
-			environmentKey;
-	int		i;
-	char	valueName[2560],
-			value[10240];
-	DWORD	nameSize,
-			valueSize;
-	DWORD	type;
-	LONG	errCode;
+	Registry	shellFolderKey,
+				environmentKey;
 
-	char	*temp = getenv( "TEMP" );
-	char	*username = getenv( "USERNAME" );
-
-	RegOpenKey(
-		HKEY_CURRENT_USER,
-		"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders",
-		&shellFolderKey
+	shellFolderKey.openPrivate(
+		"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders"
 	);
-	RegOpenKey( HKEY_CURRENT_USER, "Environment", &environmentKey );
+	environmentKey.openPrivate( "Environment", KEY_ALL_ACCESS );
 
-	i=0;
-	while( 1 )
+	RegValuePairs values;
+	shellFolderKey.getValuePairs( &values );
+	for( 
+		RegValuePairs::const_iterator it = values.cbegin(), endIT = values.cend();
+		it != endIT;
+		++it
+	)
 	{
-		nameSize = sizeof( valueName );
-		valueSize = sizeof( value );
-
-		errCode = RegEnumValue( shellFolderKey, i++,
-			valueName, &nameSize,
-			NULL,
-			&type,
-			value,
-			&valueSize );
-		if( errCode != ERROR_SUCCESS )
-			break;
-
-		if( type == REG_SZ || type == REG_EXPAND_SZ )
+		if( it->type == rtSTRING || it->type == rtENV )
 		{
 			char	*cpSource, *cpDest;
-			cpSource = cpDest = valueName;
+			/*
+				Remove all spaces and transform to uppercase from the shell folder
+				OK: we are modifying that string buffer. Since the new value will be shorter
+				and we do not use strlen it is OK
+			*/
+			cpSource = cpDest = const_cast<char*>(it->valueName.c_str());
 			while( *cpSource )
 			{
 				if( !isspace( *cpSource ) )
 				{
-					*cpDest++ = (char)toupper( *cpSource++ );
+					*cpDest++ = char(toupper( *cpSource++ ));
 				}
 				else
 					cpSource++;
 			}
 			*cpDest=0;
 
-			RegSetValueEx(
-				environmentKey, valueName, 0, REG_SZ, value, valueSize
-			);
+			environmentKey.setValueEx(it->valueName, it->type, it->valueBuffer, it->valueBuffer.size()+1 );
 		}
 	}
 
+	char	*temp = getenv( "TEMP" );
+	char	*username = getenv( "USERNAME" );
 	if( temp && *temp && username && *username )
 	{
 		size_t	tempLen = strlen( temp );
 		size_t	userLen = strlen( username );
 		if( tempLen <= userLen || strcmp( username, temp + tempLen-userLen ) )
 		{
-			if( tempLen + userLen + 3 < sizeof( value ) )
-			{
-				strcpy( value, temp );
-				strcat( value, DIRECTORY_DELIMITER_STRING );
-				strcat( value, username );
-				valueSize = (DWORD)(strlen( value ));
-				RegSetValueEx(
-					environmentKey, "TEMP", 0, REG_SZ, value, valueSize
-				);
-				RegSetValueEx(
-					environmentKey, "TMP", 0, REG_SZ, value, valueSize
-				);
-			}
+			STRING value = STRING(temp)+DIRECTORY_DELIMITER_STRING+username;
+
+			environmentKey.writeValue("TEMP", value );
+			environmentKey.writeValue("TMP", value );
 		}
 
 		mkdir( temp );
@@ -133,9 +112,6 @@ int main( void )
 	setenv( "HOMEDRIVE", environmentKey );
 	setenv( "HOMEPATH", environmentKey );
 	setenv( "HOMESHARE", environmentKey );
-
-	RegCloseKey( environmentKey );
-	RegCloseKey( shellFolderKey );
 
 	return 0;
 }
